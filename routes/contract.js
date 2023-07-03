0eb624fd6b582b254a16f4f70db83beb7a92331a
sofianeFeki <backofficekompar@gmail.com>
2023-07-03 15:15:47 +0200

    Update contract.js authcheck

--- routes/contract.js
+++ routes/contract.js
@@ -23,8 +23,8 @@ router.post('/contracts/export', exportData);
 
 
 router.put('/contract/update/quality/:slug/:energie', updateQuality);
- router.put('/contract/update/sav/:slug/:energie', authCheck, updateSav);
-router.put('/contract/update/wc/:slug/:energie', authCheck, updateWc);
+ router.put('/contract/update/sav/:slug/:energie',  updateSav);
+router.put('/contract/update/wc/:slug/:energie',  updateWc);
 
 
 
